8e2e34ccd3f66f5787a6facfd8c1769de9cdce73
mariykakameniuk-dev <mariyka.kameniuk@gmail.com>
2026-05-08 21:02:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -178,7 +178,7 @@ function createPetal(){
   petal.classList.add("petal");
 
   petal.style.left = Math.random() * 100 + "vw";
-  petal.style.animationDuration = (2 + Math.random() * 4) + "s";
+  petal.style.animationDuration = (6 + Math.random() * 5) + "s";
   petal.style.width = (10 + Math.random() * 10) + "px";
   petal.style.height = petal.style.width;
   petal.style.opacity = (0.4 + Math.random() * 0.6);
