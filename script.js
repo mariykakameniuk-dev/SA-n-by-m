fde6daec65805e38aae8b376dd29307e69f660c6
mariykakameniuk-dev <mariyka.kameniuk@gmail.com>
2026-05-08 12:21:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,37 +5,37 @@ const columnsSelect = document.getElementById("columnsSelect");
 const items = [
 
 /* КРЕМ */
-{name:"Крем + Максимайзер",tags:"крем",desc:"Склад: крем + максимайзер",oldPrice: 958,price: 858,icon:"🧴💋",img:"images/krem-max.jpg"},
-{name:"3 Крема",tags:"крем",desc:"Склад: 3 крема",oldPrice: 1077,price: 977,icon:"🧴🧴🧴",img:"images/3-krema.jpg"},
-{name:"Парфум10 + Крем",tags:"крем парфум",desc:"Склад: парфум 10 мл + крем",oldPrice: 1058,price: 908,icon:"🌸🧴",img:"images/parfum-krem.jpg"},
-{name:"Гідрейтер + Крем",tags:"крем гідрейтер догляд",desc:"Склад: гідрейтер + крем",oldPrice: 958,price: 798,icon:"💧🧴",img:"images/hydrater-krem.jpg"},
-{name:"Лосьйон250 + Крем",tags:"лосьйон крем",desc:"Склад: лосьйон250 + крем",oldPrice: 1158,price: 1008,icon:"🧴💚",images:["images/krem.jpg","images/losyon250.jpg"]},
-{name:"Тверде мило + Крем",tags:"мило крем",desc:"Склад: тверде мило + крем",oldPrice: 948,price: 820,icon:"🧼🧴",img:"images/soap-krem.jpg"},
-{name:"Міст + Крем",tags:"крем",desc:"Склад: міст + крем",oldPrice: 1348,price: 1150,icon:"✨🧴",img:"images/mist-krem.jpg"},
+{name:"Крем + Максимайзер",tags:"крем",desc:" крем + максимайзер",oldPrice: 958,price: 858,icon:"🧴💋",img:"images/krem-max.jpg"},
+{name:"3 Крема",tags:"крем",desc:" 3 крема",oldPrice: 1077,price: 977,icon:"🧴🧴🧴",img:"images/3-krema.jpg"},
+{name:"Парфум10 + Крем",tags:"крем парфум",desc:" парфум 10 мл + крем",oldPrice: 1058,price: 908,icon:"🌸🧴",img:"images/parfum-krem.jpg"},
+{name:"Гідрейтер + Крем",tags:"крем гідрейтер догляд",desc:" гідрейтер + крем",oldPrice: 958,price: 798,icon:"💧🧴",img:"images/hydrater-krem.jpg"},
+{name:"Лосьйон250 + Крем",tags:"лосьйон крем",desc:" лосьйон250 + крем",oldPrice: 1158,price: 1008,icon:"🧴💚",images:["images/krem.jpg","images/losyon250.jpg"]},
+{name:"Тверде мило + Крем",tags:"мило крем",desc:" тверде мило + крем",oldPrice: 948,price: 820,icon:"🧼🧴",img:"images/soap-krem.jpg"},
+{name:"Міст + Крем",tags:"крем",desc:" міст + крем",oldPrice: 1348,price: 1150,icon:"✨🧴",img:"images/mist-krem.jpg"},
 
 /* ПАРФУМИ */
-{name:"Парфум10 + Максимайзер",tags:"парфум",desc:"Склад: парфум10 + максимайзер",oldPrice: 1258,price: 1108,icon:"🌸💋",images:["images/maximazer.jpg","images/parfum10.jpg"]},
-{name:"Парфум10 + Парфум50",tags:"парфум",desc:"Склад: парфум 10 мл + парфум 50 мл",oldPrice: 2898,price: 2648,icon:"🌸💫",images:["images/parfum10.jpg","images/parfum50.jpg"]},
-{name:"Міст + Парфум10",tags:"парфум",desc:"Склад: міст + парфум",oldPrice: 1898,price: 1700,icon:"✨🌸",img:"images/mist-parfum.jpg"},
-{name:"Міксологія",tags:"парфум",desc:"Склад: 5 парфумів 10 мл",oldPrice: 3495,price: 2799,icon:"🌺✨",img:"images/mixology.jpg"},
+{name:"Парфум10 + Максимайзер",tags:"парфум",desc:" парфум10 + максимайзер",oldPrice: 1258,price: 1108,icon:"🌸💋",images:["images/maximazer.jpg","images/parfum10.jpg"]},
+{name:"Парфум10 + Парфум50",tags:"парфум",desc:" парфум 10 мл + парфум 50 мл",oldPrice: 2898,price: 2648,icon:"🌸💫",images:["images/parfum10.jpg","images/parfum50.jpg"]},
+{name:"Міст + Парфум10",tags:"парфум",desc:" міст + парфум",oldPrice: 1898,price: 1700,icon:"✨🌸",img:"images/mist-parfum.jpg"},
+{name:"Міксологія",tags:"парфум",desc:" 5 парфумів 10 мл",oldPrice: 3495,price: 2799,icon:"🌺✨",img:"images/mixology.jpg"},
 
 /* ДІМ */
-{name:"2 Дифузора",tags:"диф",desc:"Склад: 2 дифузора",oldPrice: 1898,price: 1698,icon:"🏠🏠",img:"images/2-dif.jpg"},
-{name:"Рефіл + Дифузор",tags:"диф",desc:"Склад: рефіл + дифузор",oldPrice: 1948,price: 1748,icon:"♻️🏠",img:"images/refill-dif.jpg"},
-{name:"Свічка + Дифузор",tags:"диф",desc:"Склад: свічка + дифузор",oldPrice: 1848,price: 1640,icon:"🕯️🏠",images:["images/svichka.jpg","images/dif.jpg"]},
-{name:"Аромат в авто + Дифузор",tags:"диф",desc:"Склад: аромат в авто + дифузор",oldPrice: 1748,price: 1540,icon:"🚗🏠",img:"images/car-dif.jpg"},
-{name:"Мило500 + Дифузор",tags:"мило диф",desc:"Склад: мило + дифузор",oldPrice: 1648,price: 1498,icon:"🧼🏠",img:"images/soap-dif.jpg"},
-{name:"8 Дифузорів",tags:"диф",desc:"Склад: 8 дифузорів",oldPrice: 8541,price: 6643,icon:"🏠✨",img:"images/8-dif.jpg"},
-{name:"Рефіл + 2 Дифузора",tags:"диф",desc:"Склад: рефіл + 2 дифузора",oldPrice: 2897,price: 2552,icon:"♻️🏠🏠",img:"images/refill-2dif.jpg"},
+{name:"2 Дифузора",tags:"диф",desc:" 2 дифузора",oldPrice: 1898,price: 1698,icon:"🏠🏠",img:"images/2-dif.jpg"},
+{name:"Рефіл + Дифузор",tags:"диф",desc:" рефіл + дифузор",oldPrice: 1948,price: 1748,icon:"♻️🏠",img:"images/refill-dif.jpg"},
+{name:"Свічка + Дифузор",tags:"диф",desc:" свічка + дифузор",oldPrice: 1848,price: 1640,icon:"🕯️🏠",images:["images/svichka.jpg","images/dif.jpg"]},
+{name:"Аромат в авто + Дифузор",tags:"диф",desc:" аромат в авто + дифузор",oldPrice: 1748,price: 1540,icon:"🚗🏠",img:"images/car-dif.jpg"},
+{name:"Мило500 + Дифузор",tags:"мило диф",desc:" мило + дифузор",oldPrice: 1648,price: 1498,icon:"🧼🏠",img:"images/soap-dif.jpg"},
+{name:"8 Дифузорів",tags:"диф",desc:" 8 дифузорів",oldPrice: 8541,price: 6643,icon:"🏠✨",img:"images/8-dif.jpg"},
+{name:"Рефіл + 2 Дифузора",tags:"диф",desc:" рефіл + 2 дифузора",oldPrice: 2897,price: 2552,icon:"♻️🏠🏠",img:"images/refill-2dif.jpg"},
 
 /* ТІЛО */
-{name:"Лосьйон250 + Гель250 + Крем",tags:"лосьйон250 + гель250 + крем",desc:"Склад: лосьйон + гель + крем",oldPrice: 1907,price: 1707,icon:"💚🧴",img:"images/body-set1.jpg"},
-{name:"Гель100 + Лосьйон100",tags:"лосьйон гель",desc:"Склад: гель100 + лосьйон100",oldPrice: 948,price: 748,icon:"💚💧",img:"images/body-set2.jpg"},
-{name:"Лосьйон250 + Гель250",tags:"лосьйон гель",desc:"Склад: лосьйон250 + гель250",oldPrice: 1498,price: 1248,icon:"💚✨",img:"images/body-set3.jpg"},
-{name:"Інтим250 + Гель250",tags:"гель",desc:"Склад: інтим250 + гель250",oldPrice: 1498,price: 1248,icon:"💗💧",img:"images/intim-gel.jpg"},
-{name:"Део + Гель250",tags:"гель",desc:"Склад: део + гель250",oldPrice: 1448,price: 1270,icon:"🌿💧",img:"images/deo-gel.jpg"},
+{name:"Лосьйон250 + Гель250 + Крем",tags:"лосьйон250 + гель250 + крем",desc:" лосьйон + гель + крем",oldPrice: 1907,price: 1707,icon:"💚🧴",img:"images/body-set1.jpg"},
+{name:"Гель100 + Лосьйон100",tags:"лосьйон гель",desc:" гель100 + лосьйон100",oldPrice: 948,price: 748,icon:"💚💧",img:"images/body-set2.jpg"},
+{name:"Лосьйон250 + Гель250",tags:"лосьйон гель",desc:" лосьйон250 + гель250",oldPrice: 1498,price: 1248,icon:"💚✨",img:"images/body-set3.jpg"},
+{name:"Інтим250 + Гель250",tags:"гель",desc:" інтим250 + гель250",oldPrice: 1498,price: 1248,icon:"💗💧",img:"images/intim-gel.jpg"},
+{name:"Део + Гель250",tags:"гель",desc:" део + гель250",oldPrice: 1448,price: 1270,icon:"🌿💧",img:"images/deo-gel.jpg"},
 {name:"3 Крема + 3 Лосьйони + 3 Гелі",tags:"3 крема + 3 лосьйона + 3 геля",desc:"Великий набір догляду",oldPrice: 5841,price: 4591,icon:"💖🧴",img:"images/mega-set.jpg"},
-{name:"Тверде мило + Лосьйон250",tags:"мило лосьйон",desc:"Склад: тверде мило + лосьйон250",oldPrice: 1348,price: 1170,icon:"🧼💚",images:["images/tverdemylo.jpg","images/losyon250.jpg"]},
+{name:"Тверде мило + Лосьйон250",tags:"мило лосьйон",desc:" тверде мило + лосьйон250",oldPrice: 1348,price: 1170,icon:"🧼💚",images:["images/tverdemylo.jpg","images/losyon250.jpg"]},
 
 /* 🌸 ВЕСНА */
 {name:"Подарунковий набір ДОМАШНІЙ ЗАТИШОК",tags:"spring дім диф свічка",holiday:"spring",desc:" диф ожина + спрей ожина + мило500 інжир + косметичка чорний",oldPrice: 3296,price: 2669,icon:"🏠✨",img:"images/home-cozy.jpg"},
